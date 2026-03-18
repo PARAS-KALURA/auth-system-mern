a29bd5ff9d31cf9240a0c3cde4f0d33e974e6273
Paras Kalura <officialkalura@gmail.com>
2026-03-19 02:12:28 +0530

    Route tested

---
+++ backend/controllers/authController.js
@@ -0,0 +1,45 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+// @desc Register user
+// @route POST /api/auth/register
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields required" });
+}
+
+        // 1️⃣ Check if user exists
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // 2️⃣ Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // 3️⃣ Create user
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        // 4️⃣ Save to DB
+        await user.save();
+
+        // 5️⃣ Response
+        res.status(201).json({
+            message: "User registered successfully"
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { registerUser };
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,22 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const connectDB =  require("./config/db");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
-
-app.use(cors());
+// Middleware
 app.use(express.json());
 
-connectDB();
-
-app.get("/", (req,res) => {
-    res.send("API is running")
-} )
+// Routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-    
+    console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app
