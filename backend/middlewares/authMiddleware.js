232c2adc80add909ab19b51a36ef9cc506e19bc4
Paras Kalura <officialkalura@gmail.com>
2026-04-03 03:24:23 +0530

    Middleware Updated, Backend Completed

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,26 +1,30 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const User = require("../models/User")
 
-const protect = (req, res, next) => {
+const protect =  async(req, res, next) => {
    
     
     // check if token exists
     try {
-     if(!req.headers.authorization) {
+     if(!req.headers.authorization?.startsWith("Bearer")) {
         return res.status(401).json({message: "No token provided"})
      }
     
      // get token
       const token = req.headers.authorization.split(" ")[1];
 
-      console.log("TOKEN:", token);
-      console.log("SECRET:", process.env.JWT_SECRET);
-
 
       // verify token
       const decode = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = decode;
+      const user = await User.findById(decode.id).select("-password");
+
+if (!user) {
+  return res.status(401).json({ message: "User not found" });
+}
+
+req.user = user;
 
       next();
 
