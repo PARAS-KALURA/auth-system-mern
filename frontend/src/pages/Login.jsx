f02989498f0aa51630da9a4a1766b8b0ef0125b9
Paras Kalura <officialkalura@gmail.com>
2026-04-03 21:36:32 +0530

    Login Page Updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='flex min-h-screen'>
+    <div className='flex min-h-screen  bg-gradient-to-r from-[#524be6] to-[#7d87f8]'>
+      
 
-     <div className='w-1/2 bg-gradient-to-r from-[#524be6] to-[#6564ee] min-h-screen flex items-center justify-center' >Left</div>
+      {/* Left Div */}
+     <div className='w-1/2 min-h-screen flex items-center justify-center' >
+     <div className='flex flex-col' >
+     <h1 className='text-6xl text-white w-full text-center p-5 font-bold' >Sample</h1>
+     <p className='text-white/80 text-sm leading-relaxed pl-10 max-w-md' >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque dolor necessitatibus quas odio, assumenda fugiat molestiae, aliquam quaerat officiis quam repellat rem dicta, voluptatem nostrum officia delectus sit maiores consectetur non consequatur expedita. Temporibus quo cum iusto quod, placeat praesentium!</p>
+     </div>
+     </div>
       
       {/* Right Div */}
-     <div className='w-1/2 bg-[#7d87f8] min-h-screen flex  justify-center items-end ' >
-     <div className='bg-white w-[370px] h-[480px] rounded-t-xl p-8' >
+     <div className='w-1/2 min-h-screen flex  justify-center items-end' >
+     <div className='bg-white w-[370px] h-[480px] rounded-t-xl p-8 shadow-[0_30px_80px_rgba(0,0,0,0.25)] hover:shadow-[0_30px_80px_rgba(0,0,0,0.40)] transition-all duration-300' >
       <h2 className='text-2xl text-center font-semibold' >Welcome Back</h2>
       <p className='text-gray-600 text-[10px] text-center pt-2 pb-6' >Let get started wit hyour 30 days free trial</p>
       <input type="text" placeholder='Username' 
@@ -29,8 +36,8 @@ const Login = () => {
 
 
       <div className='flex justify-between' >
-        <div className='px-8 py-2 bg-[#d8d8d8] rounded-3xl text-blue-600 font-bold cursor-pointer' >Google</div>
-        <div className='px-8 py-2 bg-[#d8d8d8] rounded-3xl text-blue-600 font-bold cursor-pointer' >Facebook</div>
+        <div className='px-8 py-2 bg-gradient-to-r from-purple-400 to-purple-500 hover:bg-purple-700 text-white rounded-3xl  font-bold cursor-pointer active:scale-90 active:bg-purple-800 transition-all duration-100' >Google</div>
+        <div className='px-8 py-2  rounded-3xl bg-gradient-to-r from-purple-400 to-purple-500 text-white font-bold cursor-pointer active:scale-90 active:bg-purple-800 transition-all duration-100' >Facebook</div>
       </div>
 
       <div className='flex mt-4 gap-1 items-center justify-center w-full' >
